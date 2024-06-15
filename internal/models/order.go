6566411dbcceb1e16ae46757c855408bedc66b44
vadim <lociX55Lim@gmail.com>
2024-06-15 20:45:08 +0300

    feat: add model for order

--- internal/models/order.go
+++ internal/models/order.go
@@ -1,3 +1,17 @@
 package models
 
-type Order struct{}
+import "time"
+
+type Order struct {
+	OrderUID          string    `json:"order_uid"`
+	TrackNumber       string    `json:"track_number"`
+	Entry             string    `json:"entry"`
+	Locale            string    `json:"locale"`
+	InternalSignature string    `json:"internal_signature"`
+	CustomerID        string    `json:"customer_id"`
+	DeliveryService   string    `json:"delivery_service"`
+	Shardkey          string    `json:"shardkey"`
+	SmID              int       `json:"sm_id"`
+	DateCreated       time.Time `json:"date_created"`
+	OofShard          string    `json:"oof_shard"`
+}
