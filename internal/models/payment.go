121e86986323c857a16152bb22c13a02b6cdbb0d
vadim <lociX55Lim@gmail.com>
2024-06-15 20:45:58 +0300

    feat: add model for payment

--- internal/models/payment.go
+++ internal/models/payment.go
@@ -1,3 +1,16 @@
 package models
 
-type Payment struct{}
+type Payment struct {
+	PaymentID    int     `json:"payment_id"`
+	OrderUID     string  `json:"order_uid"`
+	RequestID    string  `json:"request_id"`
+	Currency     string  `json:"currency"`
+	Provider     string  `json:"provider"`
+	Amount       float64 `json:"amount"`
+	PaymentDt    int64   `json:"payment_dt"`
+	Bank         string  `json:"bank"`
+	DeliveryCost float64 `json:"delivery_cost"`
+	GoodsTotal   float64 `json:"goods_total"`
+	CustomFee    float64 `json:"custom_fee"`
+	Transaction  string  `json:"transaction"`
+}
