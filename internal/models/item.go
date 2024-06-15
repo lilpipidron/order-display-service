f30a9167ec6458a957f04f9ec842ee228cdd16a3
vadim <lociX55Lim@gmail.com>
2024-06-15 20:38:28 +0300

    feat: add model for item

--- internal/models/item.go
+++ internal/models/item.go
@@ -1,3 +1,15 @@
 package models
 
-type Item struct{}
+type Item struct {
+	ChrtID      int     `json:"chrt_id"`
+	TrackNumber string  `json:"track_number"`
+	Price       float64 `json:"price"`
+	Rid         string  `json:"rid"`
+	Name        string  `json:"name"`
+	Sale        float64 `json:"sale"`
+	Size        string  `json:"size"`
+	TotalPrice  float64 `json:"total_price"`
+	NmID        int     `json:"nm_id"`
+	Brand       string  `json:"brand"`
+	Status      int     `json:"status"`
+}
